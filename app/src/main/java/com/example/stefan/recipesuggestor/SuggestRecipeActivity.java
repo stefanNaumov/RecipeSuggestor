1f94c1e6a5c18464bf2bca2c7a674a158b492a42
stefanNaumov <stefannaumow@gmail.com>
2014-12-01 20:52:11 +0200

    Suggest recipe UI added

--- app/src/main/java/com/example/stefan/recipesuggestor/SuggestRecipeActivity.java
+++ app/src/main/java/com/example/stefan/recipesuggestor/SuggestRecipeActivity.java
@@ -4,14 +4,39 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import Tasks.KeyboardHider;
+import Tasks.TypeWriterAnimator;
 
 
-public class SuggestRecipeActivity extends Activity {
+public class SuggestRecipeActivity extends Activity implements View.OnTouchListener{
 
+    private TypeWriterAnimator mAvailableIngredientsTypeAnimator;
+    private KeyboardHider mKeyBoardHider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggest_recipe);
+
+        this.init();
+    }
+
+    private void init(){
+        this.mAvailableIngredientsTypeAnimator =
+                (TypeWriterAnimator)findViewById(R.id.suggestRecipeTitleTypeWriterAnimatorId);
+        this.mAvailableIngredientsTypeAnimator.animateText("What do I have available..");
+
+        this.mKeyBoardHider = new KeyboardHider(this);
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.suggestRecipeLayoutId);
+       /* the two list views for available ingredients and suggested recipes are in a sub-layout -
+        hide keyboard when user touches it too*/
+        LinearLayout subLayout = (LinearLayout)findViewById(R.id.suggestRecipeSubLayoutId);
+        layout.setOnTouchListener(this);
+        subLayout.setOnTouchListener(this);
     }
 
 
@@ -35,5 +60,11 @@ public class SuggestRecipeActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        mKeyBoardHider.hideKeyobard(view);
+        return false;
     }
 }
